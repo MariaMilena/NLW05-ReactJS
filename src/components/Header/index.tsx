fc62151c8dc0378c435d89d8f313673671df2aa7
MariaMilena <milena20182018@gmail.com>
2021-05-07 19:31:50 -0300

    Implementacao do Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import format from 'date-fns/format';
+import ptBR from 'date-fns/locale/pt-BR';
+
+import styles from './styles.module.scss';
+
+export function Header () {
+    const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
+        locale: ptBR
+    });
+
+    return (
+        <header className={styles.headerContainer}>
+            <img src="/logo.jpg" alt="Podcastr" />
+            <h2>Podcastr</h2>
+
+            <p>O melhor para você ouvir, sempre</p>
+
+            <span>{currentDate}</span>
+        </header>
+    );
+}
